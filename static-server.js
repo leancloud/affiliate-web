47dfbda4e531f3713dcdee9212c63963f9c2f69a
leeyeh <lee55962698@gmail.com>
2018-08-22 14:12:13 +0800

    fix: 修复 LeanEngine 云函数信息不合法

--- static-server.js
+++ static-server.js
@@ -1,12 +1,14 @@
 var express = require('express');
 var app = express();
 
+const NOT_FOUND = (req, res) => res.status(404).send('Not found');
+
 app.use(express.static('.'));
+// Ignore cloud functions
+app.get('/1.1/functions/_ops/metadatas', NOT_FOUND);
 app.get(/.*/, function root(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
-app.use(function(req, res, next) {
-  res.status(404).send('Not found');
-});
+app.use(NOT_FOUND);
 
 app.listen(parseInt(process.env.LC_APP_PORT || 3000, 10));
